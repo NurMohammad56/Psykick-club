067395ed8dc8a6ef2dee210df07396485c3acf28
NurMohammad56 <milonsardar56@gmail.com>
2025-03-15 09:27:32 +0600

    feat: implement password recovery and reset functionality for admin

--- src/controller/admin.controller.js
+++ src/controller/admin.controller.js
@@ -1,42 +1,185 @@
-import {generateAccessAndRefreshTokens} from "../controller/user.controller.js"
-import {User} from "../model/user.model.js"
+import { generateAccessAndRefreshTokens } from "../controller/user.controller.js";
+import { User } from "../model/user.model.js";
+import { sendMail } from "../utils/email.util.js";
+import { generateOTP } from "../utils/otp.util.js";
 
 // Admin login controller
 const adminLogin = async (req, res) => {
-    const { email, password } = req.body;
-  
-    try {
-      const user = await User.findOne({ email, role: "admin" });
-      if (!user) {
-        return res
-          .status(404)
-          .json({ status: false, message: "Admin not found" });
-      }
-  
-      const isPasswordValid = await user.isPasswordValid(password);
-      if (!isPasswordValid) {
-        return res
-          .status(401)
-          .json({ status: false, message: "Password invalid" });
-      }
-  
-      const { accessToken } = await generateAccessAndRefreshTokens(user._id);
-  
-      await user.save();
-  
-      res.setHeader("Authorization", `Bearer ${accessToken}`);
-  
-      return res.status(200).json({
-        status: true,
-        message: "Admin login successful",
-        accessToken,
-      });
-    } catch (error) {
-      console.log("Error while login in admin dashboard: ", error);
-      return res
-        .status(500)
-        .json({ message: "Server error", error: error.message });
-    }
-  };
+  const { email, password } = req.body;
 
-  export { adminLogin };
+  try {
+    const user = await User.findOne({ email, role: "admin" });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ status: false, message: "Admin not found" });
+    }
+
+    const isPasswordValid = await user.isPasswordValid(password);
+    if (!isPasswordValid) {
+      return res
+        .status(401)
+        .json({ status: false, message: "Password invalid" });
+    }
+
+    const { accessToken } = await generateAccessAndRefreshTokens(user._id);
+
+    await user.save();
+
+    res.setHeader("Authorization", `Bearer ${accessToken}`);
+
+    return res.status(200).json({
+      status: true,
+      message: "Admin login successful",
+      accessToken,
+    });
+  } catch (error) {
+    console.log("Error while login in admin dashboard: ", error);
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error.message });
+  }
+};
+
+// Sent otp in email
+const forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+    if (!email) {
+      return res
+        .status(400)
+        .json({ status: false, message: "Email is required" });
+    }
+
+    // Find user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ status: false, message: "User not found" });
+    }
+
+    // Generate a random 6-digit otp
+    const otp = generateOTP();
+    const otpExpires = new Date(Date.now() + 10 * 60 * 1000);
+
+    // set the otp in database
+    user.otp = otp;
+    user.otpExpiration = otpExpires;
+    await user.save();
+
+    // Send the OTP to the users
+    await sendMail(email, otp);
+
+    return res.status(201).json({
+      status: true,
+      message: "Your OTP has been sent",
+    });
+  } catch (error) {
+    console.error("Error in forgotPassword:", error);
+    return res.status(500).json({ status: false, message: error.message });
+  }
+};
+
+// Vrify OTP
+const verifyOtp = async (req, res) => {
+  try {
+    const { otp } = req.body;
+
+    // check the user and the otp is inalid or expire
+    const user = await User.findOne({ otp });
+    if (user.otp !== otp || new Date() > user.otpExpiration) {
+      return res
+        .status(400)
+        .json({ status: false, message: "Invalid or expired OTP" });
+    }
+
+    // If verified then undefine otp details from database
+    user.otp = undefined;
+    user.otpExpires = undefined;
+    await user.save();
+
+    return res.json({ status: true, message: "OTP verified successfully" });
+  } catch (error) {
+    console.log("Error in verifyOtp:", error);
+    return res.status(500).json({ status: false, message: error.message });
+  }
+};
+
+// Resend OTP
+const resendOTP = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    if (!email) {
+      return res.status(400).json({
+        status: false,
+        message: "Email is required to resend OTP",
+      });
+    }
+
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        status: false,
+        message: "User not found",
+      });
+    }
+
+    // Generate new OTP and expiration time
+    const newOtp = generateOTP();
+    const otpExpires = new Date(Date.now() + 10 * 60 * 1000);
+
+    // Update OTP in the database
+    user.otp = newOtp;
+    user.otpExpiration = otpExpires;
+    await user.save();
+
+    await sendMail(user.email, newOtp);
+
+    return res.status(200).json({
+      status: true,
+      message: "OTP has been resent successfully",
+    });
+  } catch (error) {
+    console.error("Error while resending OTP:", error);
+    return res.status(500).json({ status: false, message: error.message });
+  }
+};
+
+// Reset pass
+const resetPassword = async (req, res) => {
+  try {
+    const { newPassword, confirmPassword } = req.body;
+
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({
+        status: false,
+        message: "User not found",
+      });
+    }
+
+    // check if new password and confirm new password match
+    if (newPassword !== confirmPassword) {
+      return res.status(400).json({
+        status: false,
+        message: "New password and confirm new password does not match",
+      });
+    }
+
+    // update the password
+    user.password = newPassword;
+    await user.save();
+
+    return res.status(200).json({
+      status: true,
+      message: "Password changed successfully",
+    });
+  } catch (error) {
+    console.log("Error while reseting password: ", error);
+    return res.status(500).json({ status: false, message: error.message });
+  }
+};
+export { adminLogin, forgotPassword, verifyOtp, resendOTP, resetPassword };
