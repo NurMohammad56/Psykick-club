41edbe8c24d6bb779fa54b1a113864a3359ff27f
NurMohammad56 <milonsardar56@gmail.com>
2025-03-16 11:00:33 +0600

    feat: add category-wise image upload functionality and update category model

--- src/route/categoryImage.route.js
+++ src/route/categoryImage.route.js
@@ -1,12 +1,23 @@
-import express from "express"
-import {createCategory} from "../controller/category.controller.js";
-import {verifyJWT} from "../middleware/auth.middleware.js"
-import {isAdmin} from "../middleware/role.middleware.js"
+import express from "express";
+import {
+  createCategory,
+  categoryWiseImageUpload,
+} from "../controller/category.controller.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
+import { isAdmin } from "../middleware/role.middleware.js";
+import upload from "../middleware/multer.middleware.js";
 
 const router = express.Router();
 
 // Create Category Route
 
 router.post("/create", verifyJWT, isAdmin, createCategory);
+router.post(
+  "/upload-category-image",
+  upload.single("image"),
+  verifyJWT,
+  isAdmin,
+  categoryWiseImageUpload
+);
 
-export default router
+export default router;
