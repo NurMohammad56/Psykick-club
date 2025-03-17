e9df829862b8ac7df4f40a5bfc55b19968086d2b
NavidulHoque <hnavidul@gmail.com>
2025-03-17 10:33:51 +0600

    feat: refactor TMC and ARV target controllers to use next for error handling; add user submission TMC controller

--- src/middleware/errorHandler.middleware.js
+++ src/middleware/errorHandler.middleware.js
@@ -1,7 +1,7 @@
 const errorHandler = (error, req, res, next) => {
 
     console.error(error);
-    
+
     return res.status(500).json({
         message: error.message
     });
--- src/route/userSubmissionTMC.route.js
+++ src/route/userSubmissionTMC.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 import { isAdmin } from '../middleware/role.middleware.js';
+import { get } from "mongoose";
 
 const router = express.Router();
 
-router.post("/create-userSubmissionTMC", verifyJWT, isAdmin,)
-router.get("/get-userSubmissionTMC-calculatePoints/:id", verifyJWT,)
+router.post("/create-userSubmissionTMC", verifyJWT, createUserSubmissionTMC);
+router.get("/get-userSubmissionTMC-calculatePoints/:id", verifyJWT, getUserSubmissionTMCAndCalculatePoints);
 
 export default router;
