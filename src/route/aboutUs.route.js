ce3dbda87ddfc65be3a2593563560663fee86d1e
NurMohammad56 <milonsardar56@gmail.com>
2025-03-18 14:02:02 +0600

    feat: add route to get About Us information

--- src/route/aboutUs.route.js
+++ src/route/aboutUs.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createAboutUs } from "../controller/aboutUs.controller.js";
+import { createAboutUs, getAboutUs } from "../controller/aboutUs.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 import { isAdmin } from "../middleware/role.middleware.js";
 
@@ -7,5 +7,7 @@ const router = express.Router();
 
 // CRUD About us
 router.post("/create-aboutUs", verifyJWT, isAdmin, createAboutUs);
+router.get("/get-aboutUs", verifyJWT, isAdmin, getAboutUs);
+
 
 export default router;
