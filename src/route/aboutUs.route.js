827781e4f7012853e237afb8ac0045aa97e28adb
NurMohammad56 <milonsardar56@gmail.com>
2025-03-18 14:32:48 +0600

    feat: get aboutUs from user

--- src/route/aboutUs.route.js
+++ src/route/aboutUs.route.js
@@ -5,11 +5,14 @@ import { isAdmin } from "../middleware/role.middleware.js";
 
 const router = express.Router();
 
-// CRUD About us
+// CRUD About us for admin
 router.post("/create-aboutUs", verifyJWT, isAdmin, createAboutUs);
 router.get("/get-aboutUs", verifyJWT, isAdmin, getAboutUs);
 router.patch("/update-aboutUs/:id", verifyJWT, isAdmin, updateAboutUs);
 router.delete("/delete-aboutUs/:id", verifyJWT, isAdmin, deleteAboutUs);
+
+// getting about us from user
+router.get("/aboutUs",verifyJWT, getAboutUs);
 
 
 export default router;
