ede06d533d7243a9a9a7efb48d042a3166a89117
NurMohammad56 <milonsardar56@gmail.com>
2025-03-18 12:05:35 +0600

    feat: add updatePrivacyPolicy function and enhance error handling in privacy policy controller

--- src/route/privacyPolicy.route.js
+++ src/route/privacyPolicy.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createPrivacyPolicy, getPrivacyPolicies} from "../controller/privacyPolicy.controller.js"
+import {createPrivacyPolicy, getPrivacyPolicies, updatePrivacyPolicy} from "../controller/privacyPolicy.controller.js"
 import {verifyJWT} from "../middleware/auth.middleware.js"
 import {isAdmin} from "../middleware/role.middleware.js"
 
@@ -8,6 +8,7 @@ const router = express.Router();
 // CRUD Privacy Policy Route from admin
 router.post("/create-privacy-policy", verifyJWT, isAdmin, createPrivacyPolicy)
 router.get("/get-privacy-policies", getPrivacyPolicies)
+router.patch("/update-privacy-policy/:id", verifyJWT, isAdmin, updatePrivacyPolicy)
 
 
 export default router;
