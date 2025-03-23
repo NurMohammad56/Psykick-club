a893151d84a3641738ce94cda68034c07c01f213
NavidulHoque <hnavidul@gmail.com>
2025-03-23 09:56:13 +0600

    feat: add analytics calculations for TMC and ARV submissions; include routes for fetching previous results

--- src/controller/userSubmission.controller.js
+++ src/controller/userSubmission.controller.js
@@ -1,6 +1,22 @@
 import { TMCTarget } from "../model/TMCTarget.model.js";
 import { ARVTarget } from "../model/ARVTarget.model.js";
 import { UserSubmission } from "../model/userSubmission.model.js";
+import { cumulativeStdNormalProbability } from 'simple-statistics';
+import { User } from "../model/user.model.js";
+
+const calculatePValue = (successfulChallenges, totalChallenges) => {
+    if (totalChallenges === 0) return 0; // Avoid division by zero
+
+    const p0 = 0.5; // Null hypothesis probability
+    const pHat = successfulChallenges / totalChallenges; // Observed success rate
+    const SE = Math.sqrt((p0 * (1 - p0)) / totalChallenges); // Standard error
+    const Z = (pHat - p0) / SE; // Z-score
+
+    // Calculate two-tailed p-value using cumulative standard normal probability
+    const pValue = 2 * (1 - cumulativeStdNormalProbability(Z))
+
+    return pValue;
+};
 
 export const createUserSubmissionTMC = async (req, res, next) => {
 
@@ -40,9 +56,7 @@ export const createUserSubmissionTMC = async (req, res, next) => {
                         id: TMCTargetId,
                         firstChoiceImage,
                         secondChoiceImage,
-                        points,
-                        successRate: 0,
-                        pValue: 0
+                        points
                     }
                 }
             },
@@ -75,17 +89,13 @@ export const createUserSubmissionARV = async (req, res, next) => {
             await newUserParticipation.save();
         }
 
-        await ARVTarget.findById(ARVTargetId)
-
         await UserSubmission.findOneAndUpdate({ userId },
             {
                 $push: {
                     participatedARVTargets: {
                         id: ARVTargetId,
                         submittedImage,
-                        points: null,
-                        successRate: 0,
-                        pValue: 0
+                        points: null
                     }
                 }
             },
@@ -93,6 +103,62 @@ export const createUserSubmissionARV = async (req, res, next) => {
 
         return res.status(201).json({
             message: "ARV challenge submitted successfully",
+        });
+    }
+
+    catch (error) {
+        next(error);
+    }
+}
+
+export const getPreviousTMCResults = async (req, res) => {
+
+    const userId = req.user._id
+    const { currentTMCTargetId } = req.params
+
+    try {
+        const userSubmission = await UserSubmission.findOne({ userId })
+
+        if (!userSubmission) {
+            return res.status(404).json({ message: "No submissions found" });
+        }
+
+        const previousTMCResults = userSubmission.participatedTMCTargets.filter(
+            target => target.id.toString() !== currentTMCTargetId
+        );
+
+        return res.status(200).json({
+            status: true,
+            message: "Previous TMC Results fetched successfully",
+            data: previousTMCResults
+        });
+    }
+
+    catch (error) {
+        next(error);
+    }
+}
+
+export const getPreviousARVResults = async (req, res) => {
+
+    const userId = req.user._id
+    const { currentARVTargetId } = req.params
+
+    try {
+        const userSubmission = await UserSubmission.findOne({ userId })
+
+        if (!userSubmission) {
+            return res.status(404).json({ message: "No submissions found" });
+        }
+
+        const previousARVResults = userSubmission.participatedARVTargets.filter(
+            target => target.id.toString() !== currentARVTargetId
+        );
+
+        return res.status(200).json({
+            status: true,
+            message: "Previous ARV Results fetched successfully",
+            data: previousARVResults
         });
     }
 
@@ -177,3 +243,47 @@ export const updateARVTargetPoints = async (req, res, next) => {
     }
 }
 
+export const updateARVAnalytics = async (req, res, next) => {
+    const userId = req.user._id
+
+    try {
+        const totalARVChallenges = await UserSubmission.countDocuments({ userId, "participatedARVTargets.points": { $ne: null } })
+        const successfulARVChallenges = await UserSubmission.countDocuments({ userId, "participatedARVTargets.points": { $gt: 0 } })
+        const successRate = (successfulARVChallenges / totalARVChallenges) * 100
+        const pValue = calculatePValue(successfulARVChallenges, totalARVChallenges)
+
+        await User.findByIdAndUpdate(userId, { ARVSuccessRate: successRate, ARVpValue: pValue })
+
+        return res.status(200).json({
+            status: true,
+            message: "ARV analytics updated successfully",
+            data: { successRate, pValue }
+        })
+    }
+
+    catch (error) {
+        next(error)
+    }
+}
+
+export const updateTMCAnalytics = async (req, res, next) => {
+    const userId = req.user._id
+
+    try {
+        const totalTMCChallenges = await UserSubmission.countDocuments({ userId, "participatedTMCTargets.points": { $ne: null } })
+        const successfulTMCChallenges = await UserSubmission.countDocuments({ userId, "participatedTMCTargets.points": { $gt: 0 } })
+        const successRate = (successfulTMCChallenges / totalTMCChallenges) * 100
+        const pValue = calculatePValue(successfulTMCChallenges, totalTMCChallenges)
+        await User.findByIdAndUpdate(userId, { TMCSuccessRate: successRate, TMCpValue: pValue })
+
+        return res.status(200).json({
+            status: true,
+            message: "TMC analytics updated successfully",
+            data: { successRate, pValue }
+        })
+    }
+
+    catch (error) {
+        next(error)
+    }
+}
