796fdb54e14e859b30ba45bcaa807c70f806c9e0
NavidulHoque <hnavidul@gmail.com>
2025-04-22 16:24:16 +0600

    feat: enhance leaderboard controllers with user details and consistent error handling

--- src/controller/leaderboard.controller.js
+++ src/controller/leaderboard.controller.js
@@ -10,25 +10,41 @@ export const getTMCLeaderboard = async (_, res, next) => {
                     tierRank: 1
                 }
             },
-            { $sort: { totalTMCPoints: -1 } }
+            { $sort: { totalTMCPoints: -1 } },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "userId",
+                    foreignField: "_id",
+                    as: "user"
+                }
+            },
+            { $unwind: "$user" },
+            {
+                $project: {
+                    totalTMCPoints: 1,
+                    tierRank: 1,
+                    user: {
+                        screenName: "$user.screenName",
+                        fullName: "$user.fullName",
+                        avatar: "$user.avatar"
+                    }
+                }
+            }
         ]);
 
         return res.status(200).json({
             status: true,
             message: "TMC Leaderboard fetched successfully",
             data: leaderboard
-        })
+        });
+    } catch (error) {
+        next(error);
     }
-
-    catch (error) {
-        next(error)
-    }
-}
+};
 
 export const getARVLeaderboard = async (_, res, next) => {
-
     try {
-
         const leaderboard = await UserSubmission.aggregate([
             {
                 $project: {
@@ -37,24 +53,44 @@ export const getARVLeaderboard = async (_, res, next) => {
                     tierRank: 1
                 }
             },
-            { $sort: { totalARVPoints: -1 } }
+            { $sort: { totalARVPoints: -1 } },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "userId",
+                    foreignField: "_id",
+                    as: "user"
+                }
+            },
+            { $unwind: "$user" },
+            {
+                $project: {
+                    totalARVPoints: 1,
+                    tierRank: 1,
+                    user: {
+                        screenName: "$user.screenName",
+                        fullName: "$user.fullName",
+                        avatar: "$user.avatar"
+                    }
+                }
+            }
         ]);
 
         return res.status(200).json({
             status: true,
             message: "ARV Leaderboard fetched successfully",
             data: leaderboard
-        })
+        });
     }
+
     catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
+
 
 export const getTotalLeaderboard = async (_, res, next) => {
-
     try {
-
         const leaderboard = await UserSubmission.aggregate([
             {
                 $project: {
@@ -68,19 +104,40 @@ export const getTotalLeaderboard = async (_, res, next) => {
                     tierRank: 1
                 }
             },
-            { $sort: { totalPoints: -1 } }
+            { $sort: { totalPoints: -1 } },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "userId",
+                    foreignField: "_id",
+                    as: "user"
+                }
+            },
+            { $unwind: "$user" },
+            {
+                $project: {
+                    totalPoints: 1,
+                    tierRank: 1,
+                    user: {
+                        screenName: "$user.screenName",
+                        fullName: "$user.fullName",
+                        avatar: "$user.avatar"
+                    }
+                }
+            }
         ]);
 
         return res.status(200).json({
             status: true,
             message: "Total Leaderboard fetched successfully",
             data: leaderboard
-        })
-    }
-
+        });
+    } 
+    
     catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
+
 
 
