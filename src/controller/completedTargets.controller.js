67ed5178697299e204a9309eebc0dd70a2213f5f
NavidulHoque <hnavidul@gmail.com>
2025-04-26 11:05:40 +0600

    refactor: remove completedTargets route and integrate completed targets functionality into admin route

--- src/controller/completedTargets.controller.js
+++ src/controller/completedTargets.controller.js
@@ -19,3 +19,22 @@ export const getAllCompletedTargets = async (_, res, next) => {
         next(error);
     }
 }
+
+export const getAllCompletedTargetsCount = async (_, res, next) => {
+    try {
+        const completedTargets = await CompletedTargets.findById(process.env.COMPLETED_TARGETS_DOCUMENT_ID);
+
+        return res.status(200).json({
+            status: true,
+            data: {
+                completedTMCCount: completedTargets?.TMCTargets?.length || 0,
+                completedARVCount: completedTargets?.ARVTargets?.length || 0,
+            },
+            message: "All completed targets count fetched successfully"
+        });
+    }
+
+    catch (error) {
+        next(error);
+    }
+}
