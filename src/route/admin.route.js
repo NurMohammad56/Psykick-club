5693c92752fbacf14d8c49cc08c2b5d78e096096
NavidulHoque <hnavidul@gmail.com>
2025-04-27 14:24:21 +0600

    fix: add authentication and authorization middleware to admin and aboutUs routes

--- src/route/admin.route.js
+++ src/route/admin.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { adminLogin, forgotPassword, verifyOtp, resendOTP, resetPassword, updateAdminProfile, changePasswordAdmin, getGameParticipationStats, getAdminProfile, getAverageSessionDuration, getProfileCompleteness, getUserSessionDurations, getAllUsers, getActiveUsersCount, getContactUs } from "../controller/admin.controller.js"
+import { adminLogin, forgotPassword, verifyOtp, resendOTP, resetPassword, updateAdminProfile, changePasswordAdmin, getGameParticipationStats, getAverageSessionDuration, getProfileCompleteness, getUserSessionDurations, getAllUsers, getActiveUsersCount, getContactUs } from "../controller/admin.controller.js"
 import { getCompletedTargets } from "../controller/userSubmission.controller.js"
 import { getAllCompletedTargets, getAllCompletedTargetsCount } from "../controller/completedTargets.controller.js"
 import { isAdmin } from "../middleware/role.middleware.js"
@@ -16,7 +16,6 @@ router.post("/reset-password", resetPassword);
 
 // Admin Profile Route
 router.patch("/profile", verifyJWT, isAdmin, upload.single("avatar"), updateAdminProfile);
-router.get("/get-profile", verifyJWT, isAdmin, getAdminProfile);
 router.get("/profile-completeness", verifyJWT, isAdmin, getProfileCompleteness);
 
 
@@ -26,26 +25,26 @@ router.patch("/change-password", verifyJWT, isAdmin, changePasswordAdmin);
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DASHBOARD>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 // Get session duration
 router.get("/session-durations/:userId", verifyJWT, isAdmin, getUserSessionDurations);
-router.get("/average-session-duration", getAverageSessionDuration);
+router.get("/average-session-duration", verifyJWT, isAdmin, getAverageSessionDuration);
 // Get all users
-router.get("/all-users", getAllUsers);
+router.get("/all-users", verifyJWT, isAdmin, getAllUsers);
 // Get active users count
-router.get("/active-users-count", getActiveUsersCount);
+router.get("/active-users-count", verifyJWT, isAdmin, getActiveUsersCount);
 
 // Get game participation stats
-router.get("/game-graph", getGameParticipationStats)
+router.get("/game-graph", verifyJWT, isAdmin, getGameParticipationStats);
 
 // Get completed targets for a user
-router.get('/completedTargets', getCompletedTargets);
+router.get('/completedTargets', verifyJWT, getCompletedTargets);
 
 //get all completed targets
-router.get('/get-all-completed-targets', getAllCompletedTargets);
+router.get('/get-all-completed-targets', verifyJWT, isAdmin, getAllCompletedTargets);
 
 //get count of total completed targets
-router.get('/get-all-completed-targets-count', getAllCompletedTargetsCount);
+router.get('/get-all-completed-targets-count', verifyJWT, isAdmin, getAllCompletedTargetsCount);
 
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<CONTACT US>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-router.get("/all-contact-us", getContactUs);
+router.get("/all-contact-us", verifyJWT, isAdmin, getContactUs);
 
 
 
