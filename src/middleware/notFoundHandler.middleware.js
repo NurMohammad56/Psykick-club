ee77299de9807b638fc2749f03984d1220f14965
NurMohammad56 <milonsardar56@gmail.com>
2025-05-01 01:20:43 +0600

    fix: Remove console log from not found handler middleware

--- src/middleware/notFoundHandler.middleware.js
+++ src/middleware/notFoundHandler.middleware.js
@@ -1,5 +1,4 @@
 export const notFoundHandler = (req, res, next) => {
-    console.log("404 route hit:", req.method, req.originalUrl);
     return res.status(404).json({
         message: "The requested resource was not found!",
     })
