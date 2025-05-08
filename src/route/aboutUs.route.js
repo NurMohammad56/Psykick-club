8cff0047069b4c08a0d245a360175b80545c78a2
NavidulHoque <hnavidul@gmail.com>
2025-05-08 16:07:58 +0600

    refactor: Remove authentication middleware from About Us and Terms & Conditions routes

--- src/route/aboutUs.route.js
+++ src/route/aboutUs.route.js
@@ -1,13 +1,11 @@
 import express from "express";
 import { createAboutUs, getAboutUs, updateAboutUs, deleteAboutUs } from "../controller/aboutUs.controller.js";
-import { verifyJWT } from "../middleware/auth.middleware.js";
-import { isAdmin } from "../middleware/role.middleware.js";
 
 const router = express.Router();
 
 // CRUD About us for admin
-router.post("/create-aboutUs", verifyJWT, isAdmin, createAboutUs);
-router.get("/get-aboutUs/:id", verifyJWT, getAboutUs);
-router.patch("/update-aboutUs/:id", verifyJWT, isAdmin, updateAboutUs);
+router.post("/create-aboutUs", createAboutUs);
+router.get("/get-aboutUs/:id", getAboutUs);
+router.patch("/update-aboutUs/:id", updateAboutUs);
 
 export default router;
