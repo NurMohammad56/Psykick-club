9b9b1492ebe657d7b9170fdb974e2ff3cba8f3a4
NavidulHoque <hnavidul@gmail.com>
2025-05-08 12:51:19 +0600

    refactor: Remove JWT verification from home counts route

--- src/route/home.route.js
+++ src/route/home.route.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { verifyJWT } from "../middleware/auth.middleware.js"
 import { getHomeCounts } from "../controller/home.controller.js";
 
 const router = express.Router();
 
 // Home page counts
-router.get("/counts", verifyJWT, getHomeCounts);
+router.get("/counts", getHomeCounts);
 
 export default router;
