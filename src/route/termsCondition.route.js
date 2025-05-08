308620028e1d773062beead1c59dff97f0358cdb
NavidulHoque <hnavidul@gmail.com>
2025-05-08 15:35:17 +0600

    refactor: Simplify terms condition routes and remove unused delete functionality

--- src/route/termsCondition.route.js
+++ src/route/termsCondition.route.js
@@ -2,8 +2,7 @@ import express from "express";
 import {
   createTermsCondition,
   getTermsCondition,
-  updateTermsCondition,
-  deleteTermsCondition,
+  updateTermsCondition
 } from "../controller/termsCondition.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 import { isAdmin } from "../middleware/role.middleware.js";
@@ -12,26 +11,17 @@ const router = express.Router();
 
 // CRUD Terms & Condition Route from admin
 router.post(
-  "/create-terms-and-condition/:previousId",
+  "/create-terms-and-condition",
   verifyJWT,
   isAdmin,
   createTermsCondition
 );
-router.get("/get-terms-and-condition", getTermsCondition);
+router.get("/get-terms-and-condition/:id", getTermsCondition);
 router.patch(
   "/update-terms-and-condition/:id",
   verifyJWT,
   isAdmin,
   updateTermsCondition
 );
-router.delete(
-  "/delete-terms-and-condition/:id",
-  verifyJWT,
-  isAdmin,
-  deleteTermsCondition
-);
-
-// Get Terms Route from user
-router.get("/terms-condition-user", verifyJWT, getTermsCondition);
 
 export default router;
