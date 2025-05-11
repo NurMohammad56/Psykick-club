22e223e25eac35395ee7f4eee8c7386a01348146
NavidulHoque <hnavidul@gmail.com>
2025-05-11 17:41:40 +0600

    refactor: Enhance getNotifications to support pagination and return total items

--- src/controller/notification.controller.js
+++ src/controller/notification.controller.js
@@ -29,19 +29,42 @@ export const createNotification = async (req, res, next) => {
 
 export const getNotifications = async (req, res, next) => {
     const { userId } = req.params;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
 
     try {
-        const notifications = await Notification.find({
-            $or: [
-                { userId },  
-                { userId: null }     
-            ]
-        }).sort({ createdAt: -1 })
+        const [notifications, totalItems] = await Promise.all([
+            await Notification
+                .find({
+                    $or: [
+                        { userId },
+                        { userId: null }
+                    ]
+                })
+                .skip(skip)
+                .limit(limit)
+                .sort({ createdAt: -1 }),
+            Notification.countDocuments({
+                $or: [
+                    { userId },
+                    { userId: null }
+                ]
+            })
+        ])
+
+        const totalPages = Math.ceil(totalItems / limit);
 
         return res.status(200).json({
             status: true,
             message: "Notifications fetched successfully",
-            data: notifications
+            data: notifications,
+            pagination: {
+                totalItems,
+                totalPages,
+                currentPage: page,
+                itemsPerPage: limit
+            }
         });
     }
 
